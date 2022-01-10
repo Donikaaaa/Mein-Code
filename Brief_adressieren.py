82e0255c2781a2048cc1ad561f88144d74ca5b38
Donikaaaa <97448653+Donikaaaa@users.noreply.github.com>
2022-01-10 14:14:41 +0100

    Add files via upload

---
+++ Brief_adressieren.py
@@ -0,0 +1,11 @@
+def funktion(Name,Nachname):
+    u = Name+Nachname
+print (funktion("Donika", "Limani"))
+
+def funktion(Strasse, Nummer)
+    f = Strasse+Nummer
+print(funktion("Nebikerstrasse" + str(77)))
+
+def funktion(Platz, Ort)
+    g = Platz + Ort
+print(funktion(str (6247) + "Schötz" ))
